4240de5b234c8d747d8a246fae102a382d3af85a
Hankang Li <67124859+authentisity@users.noreply.github.com>
2023-11-17 15:55:49 -0800

    Update api.js

--- api.js
+++ api.js
@@ -7,7 +7,7 @@ function checkLocation(response) {
   return response;
 }
 
-function fetchAPI(city) {
+export default function fetchAPI(city) {
   const KEY = '83fbb406930f7d4acd11395f2d3b66a8';
   const url = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&APPID=' + KEY + '&units=metric';
   fetch(url, {mode: 'cors'})
@@ -21,8 +21,7 @@ function fetchAPI(city) {
         console.log(loc.list[0].weather[0].description);
         console.log(loc.city.name);
         console.log(loc.list[0].weather[0].main.feels_like);
-        console.log(loc.list[4].wind.speed*3.6);
-        console.log(loc.list[0].weather[0].description);    
+        console.log((loc.list[4].wind.speed*3.6).toFixed(2));
         console.log(loc.list[0].main.humidity + '%');
         console.log(loc.list[0].main.pressure + ' hPa');    
  
